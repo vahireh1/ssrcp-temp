d84af6955638760706317814f37d70f64642fbd3
vahireh1 <vahireh121@gmail.com>
2020-05-23 12:35:12 +0530

    single template page changes

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000);
+console.log('path checking');
